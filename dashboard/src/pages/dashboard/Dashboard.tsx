f38c0552692426c212bd8827a503da2b1294ef03
MohMohAung <mohmohaung737@gmail.com>
2024-04-06 22:11:33 +0630

    fix-userList

--- dashboard/src/pages/dashboard/Dashboard.tsx
+++ dashboard/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import { PieChart } from "react-minimal-pie-chart";
+import { useAllUser } from "@/api/userList/query";
+import classes from "./Dashboard.module.css";
+import { LineChart, CartesianGrid, Line, Tooltip, XAxis } from "recharts";
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const { data, isError } = useAllUser();
 
-export default Dashboard
+  const pieChart = [
+    {
+      title: "Acitve",
+      value: data?.filter((user) => user.isActive).length || 0,
+      color: "red",
+    },
+    {
+      title: "inAcitve",
+      value: data?.filter((user) => !user.isActive).length || 0,
+      color: "green",
+    },
+  ];
+  return (
+    <div className={classes.UserListContainer}>
+      <div className={classes.UserList}>
+        <h1>Dashboard</h1>
+        <div className={classes.pieChart}>
+          <div>
+            <PieChart data={pieChart} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
