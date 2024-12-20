c3636b42f1ef476eb0ba3b7e375d3b0921fe928d
MohMohAung <mohmohaung737@gmail.com>
2024-12-20 23:35:45 +0630

    feat-category

--- ecommerce-backend/src/routes/user.js
+++ ecommerce-backend/src/routes/user.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { userById } = require("../controller/user");
-const { requireSignin } = require("../controller/auth");
+const { requireSignin, isAdmin, isAuth } = require("../controller/auth");
 
 router.get("/", (req, res) => {
   res.send("hello from node");
 });
 
-router.get("/secret/:userId", requireSignin, (req, res) => {
+router.get("/secret/:userId", requireSignin, isAdmin, isAuth, (req, res) => {
   res.json({
     user: req.profile,
   });
